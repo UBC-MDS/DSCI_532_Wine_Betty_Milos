d8fd9c65760aabd53f26bfacab5882a4fe3a1ab2
Betty Zhou <betty.zhou@alumni.ubc.ca>
2019-01-25 21:07:44 -0800

    "fixes to app to address TA feedback"

--- Wine/app.R
+++ Wine/app.R
@@ -10,7 +10,8 @@ wine <- read.csv("winemag-data-130k-v2.csv", stringsAsFactors = FALSE) %>%
 ui <- fluidPage(
   titlePanel("Wind Down the Wine", 
              windowTitle = "Wine Review App"),
-   sidebarLayout(
+    #Add sidebar Panel
+    sidebarLayout(
      sidebarPanel(
        chooseSliderSkin("HTML5", color = '#778899'),
        tags$head(tags$style(HTML( '.has-feedback .form-control { padding-right: 0px;}' ))),
@@ -19,15 +20,18 @@ ui <- fluidPage(
        sliderInput("ratingInput", "Select your desired rating range.*",
                    min = 80, max = 100, value = c(80, 85)),
        hr(),
-       span('*the lowest rating in this dataset is 80')
+       span('*the lowest rating in this dataset is 80'),
+       hr(),
+       span('Source: https://www.kaggle.com/zynicide/wine-reviews/data')
      ),
+     # Main Panel
      mainPanel(
+       # Tabs for Price and Rating Distribution
        tabsetPanel(
          tabPanel("Price Distribution", plotOutput("price_hist")),
          tabPanel("Rating Distribution",plotOutput("rating_hist"))),
        p("Table of filtered results are shown below:", style='font-size:120%'),
        dataTableOutput("table")
-                   
        )
      )
    )
@@ -38,6 +42,7 @@ server <- function(input, output) {
   
   observe(print(input$priceInput))
   
+  # filter dataset based on user input values
   wine_filtered <-  reactive(
     wine %>% 
       filter(price > input$priceInput[1],
@@ -46,16 +51,17 @@ server <- function(input, output) {
              points < input$ratingInput[2])
     )
   
+  # Price histogram
    output$price_hist <- renderPlot(
      wine_filtered() %>% 
        ggplot(aes(price), fill) + 
        geom_histogram(fill ='lightblue', colour='grey') +
        xlab("Price ($)") +
        theme_bw() +
-       theme(text = element_text(size=20)) 
-       
+       theme(text = element_text(size=20))
    )
-   
+  
+  # Rating histogram 
   output$rating_hist <- renderPlot(
     wine_filtered() %>% 
        ggplot(aes(points)) + 
@@ -63,9 +69,9 @@ server <- function(input, output) {
        xlab("Rating (out of 100)") +
        theme_bw() +
        theme(text = element_text(size=20))
-       
    )
-   
+  
+  # filtered results table 
   output$table <- renderDataTable(
     datatable(wine_filtered(), 
               caption = "Table contains filtered results based on selected price and rating ranges. 
@@ -84,7 +90,6 @@ server <- function(input, output) {
               )
     )
   )
-  
 }
 
 # Run the application 
